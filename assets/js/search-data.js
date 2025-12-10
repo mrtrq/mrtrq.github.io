1e213cfe4b32a598d38cf6c2837f5a87e2cd1019
mrtrq <69755166+mrtrq@users.noreply.github.com>
2025-12-10 16:21:53 +0000

    Deploying to gh-pages from @ mrtrq/mrtrq.github.io@a994448464944b610fcfdd1fdad07b0a93f240f7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -398,6 +398,11 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/elegant_universe/";
+            },},{id: "books-hunter-x-hunter",
+          title: 'Hunter X Hunter',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/hunter_x_hunter/";
             },},{id: "books-catatan-sang-sahabat-alam",
           title: 'Catatan Sang Sahabat Alam',
           description: "",
@@ -475,27 +480,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("mailto:%74%61%72%72%65%71.%6D%61%75%6C%61%6E%61@%67%6D%61%69%6C.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
         },
       },{
       id: 'light-theme',
